28bf26e97281f4119f485a917b0838f6109e7a04
Raymond Barakat <RaymondSBarakat@gmail.com>
2019-04-14 23:00:32 -0400

    More convincing real time analytics

--- src/ctrl/Start.java
+++ src/ctrl/Start.java
@@ -994,13 +994,6 @@ public class Start extends HttpServlet {
 		request.setAttribute("OBMResultsReady", true);
 		target = "/OrdersByMonth.jspx";
 	}
-	
-	public void getTop10(HttpServletRequest request, HttpServletResponse response, DatabaseOperator databaseOperator, ErrorChecking errorChecking) {
-		
-		ArrayList<BookBean> abb = (ArrayList<BookBean>) request.getSession().getAttribute("Top10");
-		request.getSession().setAttribute("Top10", abb);
-	}
-	
 
 	@GET
     @Path("/pcs/")
